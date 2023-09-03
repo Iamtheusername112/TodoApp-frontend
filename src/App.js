11d87fafad4fce845921f97cf2a41e4fd4fe33bc
Iamtheusername112 <apkfem@gmail.com>
2023-09-03 17:52:09 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,115 +1,137 @@
-import { useEffect, useState } from "react";
-const api_base = "https://todoapp-mgt3.onrender.com";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [popupActive, setPopupActive] = useState(false);
-  const [newTodo, setNewTodo] = useState("");
+  const [itemText, setItemText] = useState("");
+  const [listItems, setListItems] = useState([]);
+  const [isUpdating, setIsUpdating] = useState("");
+  const [updateItemText, setUpdateItemText] = useState("");
 
+  //add new todo item to database
+  const addItem = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5500/api/item", {
+        item: itemText,
+      });
+      setListItems((prev) => [...prev, res.data]);
+      setItemText("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //Create function to fetch all todo items from database -- we will use useEffect hook
   useEffect(() => {
-    GetTodos();
+    const getItemsList = async () => {
+      try {
+        const res = await axios.get("http://localhost:5500/api/items");
+        setListItems(res.data);
+        console.log("render");
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getItemsList();
   }, []);
 
-  const GetTodos = () => {
-    fetch(api_base + "/todos")
-      .then((res) => res.json())
-      .then((data) => setTodos(data))
-      .catch((err) => console.error("Error: ", err));
+  // Delete item when click on delete
+  const deleteItem = async (id) => {
+    try {
+      const res = await axios.delete(`http://localhost:5500/api/item/${id}`);
+      const newListItems = listItems.filter((item) => item._id !== id);
+      setListItems(newListItems);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  const completeTodo = async (id) => {
-    const data = await fetch(api_base + "/todo/complete/" + id).then((res) =>
-      res.json()
-    );
-
-    setTodos((todos) =>
-      todos.map((todo) => {
-        if (todo._id === data._id) {
-          todo.complete = data.complete;
-        }
-
-        return todo;
-      })
-    );
+  //Update item
+  const updateItem = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.put(
+        `http://localhost:5500/api/item/${isUpdating}`,
+        { item: updateItemText }
+      );
+      console.log(res.data);
+      const updatedItemIndex = listItems.findIndex(
+        (item) => item._id === isUpdating
+      );
+      const updatedItem = (listItems[updatedItemIndex].item = updateItemText);
+      setUpdateItemText("");
+      setIsUpdating("");
+    } catch (err) {
+      console.log(err);
+    }
   };
-
-  const addTodo = async () => {
-    const data = await fetch(api_base + "/todo/new", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        text: newTodo,
-      }),
-    }).then((res) => res.json());
-
-    setTodos([...todos, data]);
-
-    setPopupActive(false);
-    setNewTodo("");
-  };
-
-  const deleteTodo = async (id) => {
-    const data = await fetch(api_base + "/todo/delete/" + id, {
-      method: "DELETE",
-    }).then((res) => res.json());
-
-    setTodos((todos) => todos.filter((todo) => todo._id !== data.result._id));
-  };
+  //before updating item we need to show input field where we will create our updated item
+  const renderUpdateForm = () => (
+    <form
+      className="update-form"
+      onSubmit={(e) => {
+        updateItem(e);
+      }}
+    >
+      <input
+        className="update-new-input"
+        type="text"
+        placeholder="New Item"
+        onChange={(e) => {
+          setUpdateItemText(e.target.value);
+        }}
+        value={updateItemText}
+      />
+      <button className="update-new-btn" type="submit">
+        Update
+      </button>
+    </form>
+  );
 
   return (
     <div className="App">
-      <h1>Welcome!!</h1>
-      <h4>Your tasks</h4>
-
-      <div className="todos">
-        {todos.length > 0 ? (
-          todos.map((todo) => (
-            <div
-              className={"todo" + (todo.complete ? " is-complete" : "")}
-              key={todo._id}
-              onClick={() => completeTodo(todo._id)}
-            >
-              <div className="checkbox"></div>
-
-              <div className="text">{todo.text}</div>
-
-              <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>
-                x
-              </div>
-            </div>
-          ))
-        ) : (
-          <p>You currently have no tasks add some</p>
-        )}
-      </div>
-
-      <div className="addPopup" onClick={() => setPopupActive(true)}>
-        +
-      </div>
-
-      {popupActive ? (
-        <div className="popup">
-          <div className="closePopup" onClick={() => setPopupActive(false)}>
-            X
+      <h1>Todo List</h1>
+      <form className="form" onSubmit={(e) => addItem(e)}>
+        <input
+          type="text"
+          placeholder="Add Todo Item"
+          onChange={(e) => {
+            setItemText(e.target.value);
+          }}
+          value={itemText}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <div className="todo-listItems">
+        {listItems.map((item) => (
+          <div className="todo-item">
+            {isUpdating === item._id ? (
+              renderUpdateForm()
+            ) : (
+              <>
+                <p className="item-content">{item.item}</p>
+                <button
+                  className="update-item"
+                  onClick={() => {
+                    setIsUpdating(item._id);
+                  }}
+                >
+                  Update
+                </button>
+                <button
+                  className="delete-item"
+                  onClick={() => {
+                    deleteItem(item._id);
+                  }}
+                >
+                  Delete
+                </button>
+              </>
+            )}
           </div>
-          <div className="content">
-            <h3>Add Task</h3>
-            <input
-              type="text"
-              className="add-todo-input"
-              onChange={(e) => setNewTodo(e.target.value)}
-              value={newTodo}
-            />
-            <div className="button" onClick={addTodo}>
-              Create Task
-            </div>
-          </div>
-        </div>
-      ) : (
-        ""
-      )}
+        ))}
+      </div>
     </div>
   );
 }
